d9ae0c37da0b6b9d615caa4ed8abe2207dd30367
Simon Ashbery <si.ashbery@gmail.com>
2017-05-10 16:13:58 +0100

    created new test cases for failure and debugging release_bike

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -5,11 +5,16 @@ class DockingStation
 attr_reader :bike
 
   def release_bike
-    @bike = Bike.new
+    @bike = Bike.new # ? bike_available? == true : "No Bikes available"
+
   end
 
-  def dock(bike = @bike)
-    bike
+  def dock(bike)
+    @bike = bike
+  end
+
+  def bike_available?
+    true
   end
 
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -3,11 +3,12 @@ require 'DockingStation'
 describe DockingStation do
 
 	it { is_expected.to respond_to :release_bike }
-
-	it "gets a bike" do
-		@bike = subject.release_bike
-		expect(@bike).to be_working
+describe '#release_bike' do
+	it "releases a bike" do
+		@bike = Bike.new
+		expect(subject.release_bike).to eq @bike
 	end
+end
 
 	it "Docks a bike" do
 		bike = Bike.new
@@ -22,5 +23,16 @@ describe DockingStation do
 		expect(subject.bike).to eq @bike
 	end
 
+	it "responds to bike_available?" do
+		expect(subject).to respond_to :bike_available?
+	end
+
+#	it "bike available returns true" do
+#		expect(subject.bike_available?).to eq true
+#	end
+
+	#it "expect an error in bike available" do
+		#expect { subject.bike_available? }.to raise_error(RuntimeError)
+	#end
 
 end
